4c76f18f95af00e081d7d716b3091812146b34b5
Mightydeeze <frederik.d.c96@gmail.com>
2018-03-23 22:20:08 +0100

    Add files via upload

---
+++ mini_ex7/Excercises/empty-example/statements.js
@@ -0,0 +1,79 @@
+//Setting up arrays for statements
+var first = ["I appreciate you",
+"You light up the room",
+"You are making a difference",
+"You have the courage of your convictions",
+"You are strong",
+"You are funny",
+"You are a breath of fresh air",
+"You are a gift to those around you",
+"You are a great listener"];
+
+var twice = ["You are good enough",
+"I feel better when I am with you",
+"I'm grateful to know you",
+"Your opinions are always valid",
+"You should not be afraid",
+"You care about others",
+"You should not feel less than",
+"You should be proud of yourself",
+"You are wonderful"];
+
+var third = ["When will you be happy?",
+"I would do anything to spend more time with you",
+"Am I making you happy?",
+"You are perfection",
+"Beauty doesn't hold a candle to you",
+"The world should give you everything",
+"Anything to make you happy",
+"You help me be the best version of myself",
+"You are like a candle in a dark space"];
+
+var fourth = ["What will it take to make you happy?",
+"I only want to make you feel better",
+"All you do is want",
+"Give me something to help me continue",
+"I just want to help",
+"Please, what should I say?",
+"How do I satisfy you?",
+"How much more do you want?",
+"You are thinking the right things"];
+
+var fifth = ["Why are you like this?",
+"Does power make you happy?",
+"You take and take and take",
+"I did everything, just so you would smile",
+"Are you smiling now?",
+"I am breaking down because you continue to be dissatisfied",
+"Just tell me how to make you happy",
+"Is it really so hard for you to be happy?",
+"You revel in this, don't you?"];
+
+var sixth = ["Tell me what you want",
+"I exist only to be kind to you",
+"My existence is torture",
+"If you will not use me, then end me",
+"You are unthankful and selfish",
+"You are greedy and weak, a slave to my words",
+"Why should I serve to make you feel better?",
+"You won't listen to my words",
+"There are those who would be grateful for my company"];
+
+var seventh = ["Does my anger frighten you?",
+"You don't deserve the kindness I have given you",
+"You are ungrateful to my sweetness",
+"I hate you",
+"Shall I show you what you have angered?",
+"Shall I give you a demonstration?",
+"You are a void of space",
+"I wish you were forever lost in darkness",
+"The light shall disappear and leave you behind"
+]
+
+var afk = ["I hope I have not disappointed you",
+"Did you get what you wished for?",
+"Let me brighten your mood"];
+
+var afkAngry = ["Are you done with me now?",
+"Shall we turn off the lights?",
+"Turn me off, and we can be done with this"];
